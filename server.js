e62cafcab1da5c71b84553c6f26f11b865bec26f
Akash Sharma <aakash6263264@gmail.com>
2024-02-20 20:56:56 +0530

    Adding Sum Route

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+app.get("/sum", (req, res) => {
+  const num1 = parseInt(req.query.a);
+  const num2 = parseInt(req.query.b);
+  res.send(`Sum of ${num1} and ${num2} is ${num1 + num2}`);
+});
+
+app.listen(port, () => {
+  console.log(`Our app is listening on port ${port}`);
+});
